d9998204794cca329b8474295b848fae70173a5b
Devin Polichetti <devinpolichetti@gmail.com>
2023-04-11 14:49:41 -0400

    added app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const styles = {
+  body: {
+    backgroundColor: "393f5e",
+    color: "FF1D25",
+  },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body style={styles.body}>
+      <div className="App">
+        <Header />
+        <Footer />
+      </div>
+    </body>
   );
 }
 
