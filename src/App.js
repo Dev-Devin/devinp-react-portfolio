9fe6a63f9d3043ee88264efd36f86464b3610d2b
Devin Polichetti <devinpolichetti@gmail.com>
2023-04-11 16:07:26 -0400

    added styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 
 const styles = {
   body: {
