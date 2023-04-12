bc2ad3bd31a52a96dcd5eca543c2031574562198
Devin Polichetti <devinpolichetti@gmail.com>
2023-04-11 21:46:45 -0400

    added my about me in JS

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,18 +5,21 @@ import avatar from "../../assets/avatar.jpeg";
 export default function About() {
   return (
     <section className="container">
-      {/* <h2 className="title">I'M LAURA</h2> */}
-      <h3 className="subtitle">hi! i'm laura</h3>
+      {/* <h2 className="title">Devin Polichetti</h2> */}
+      <h3 className="subtitle">Devin Polichetti</h3>
       <div className="content">
         <div className="container">
           <div className="row" id="about-col">
             <div className="col-sm-6">
               <p className="about">
-                I'm a creative and solutions-driven full stack web developer who
-                recently earned a certificate in full stack development from
-                Columbia University. I have honed skills in HTML, CSS,
-                JavaScript, React.js, MySQL, and more to design and build
-                meaningful applications from ideation to production.
+                I am a talented and forward-thinking full stack web developer
+                who has gained extensive proficiency in HTML, CSS, JavaScript,
+                React.js, MySQL, and other cutting-edge technologies through
+                rigorous training at Columbia University's renowned full stack
+                development certificate program. With a strong emphasis on
+                creating meaningful applications, I am skilled in designing and
+                developing projects from concept to production, showcasing my
+                innovative and problem-solving approach.
               </p>
             </div>
             <div className="col-sm-6">
@@ -25,13 +28,17 @@ export default function About() {
           </div>
           <p className="about"></p>
           <p className="about">
-            I have 8+ years of professional experience in a variety of
-            operational roles in New York. Most recently, I served as Chief of
-            Staff to the COO at a healthcare tech startup where I spent a lot of
-            time collaborating with engineers and developers, ultimately
-            inspiring me to make a pivot. Leveraging my background in
-            operations, I am able to take users’ needs and issues, translate to
-            product solutions, and execute the technical work.
+            I'm a highly motivated full stack web developer with a
+            Bachelor's degree in Computer Science and Information Security.
+            Recently, I completed a rigorous full stack web development bootcamp
+            at Columbia University, where I gained expertise in HTML, CSS,
+            JavaScript, React.js, MySQL, and more. I'm passionate about creating
+            meaningful and user-friendly web applications, and I approach every
+            project with a creative and solutions-driven mindset. With a strong
+            educational background and recent bootcamp experience, I am
+            well-prepared to design and develop innovative web applications,
+            pushing the boundaries of what is possible in the world of web
+            development.
           </p>
         </div>
       </div>
