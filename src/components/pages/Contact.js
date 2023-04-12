d77270a69c1e01a8079902a3165de08899df8b95
Devin Polichetti <devinpolichetti@gmail.com>
2023-04-12 10:30:39 -0400

    added js

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "../../App.css";
+
+export default function Contact() {
+  return (
+    <section className="container">
+      {/* <h2 className="title">CONTACT</h2> */}
+      <h3 className="subtitle">Welcome!</h3>
+      {/* <form className='form'>
+                <input className='formName' name='name' type='text' placeholder='name'/>
+                <input className='formEmail'name='email' type='email' placeholder='email'/>
+                <input className='formMessage' name='message' type='textArea' placeholder='message'/>
+                <input className='btn btn-success formButton' type='submit' value='Send'/>
+            </form> */}
+      <div className="content" id="contact-content">
+        <p>
+          Feel free to contact me with any inquiries or potential collaboration
+          opportunities. I'm open to discussing any questions or projects you
+          may have.
+        </p>
+        <ul id="contact-list">
+          <li>
+            <b>EMAIL:</b>
+            <a href="mailto:devinpolichetti@gmail.com">
+              DEVINPOLICHETTI@GMAIL.COM
+            </a>
+          </li>
+          <li>
+            <b>ONLINE:</b>{" "}
+            <a
+              href="https://www.linkedin.com/in/devin-polichetti-8b1b1b1b4/"
+              target="_blank"
+            >
+              LINKEDIN
+            </a>{" "}
+            /{" "}
+            <a href="https://github.com/Dev-Devin" target="_blank">
+              GITHUB
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Project from "../Project";
+import "../../App.css";
+import "../../styles/Portfolio.css";
+import { project } from "../../helpers/Project";
+
+export default function Portfolio() {
+  return (
+    <div className="portfolio container">
+      {/* <h2 className='title'>MY PROJECTS</h2> */}
+      <h3 className="subtitle">Projects I have developed</h3>
+      <div className="content projectList">
+        {projectList.map((project, index) => {
+          return (
+            <Project
+              key={index}
+              name={project.name}
+              image={project.image}
+              technologies={project.technologies}
+              deployed={project.deployed}
+              github={project.github}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
